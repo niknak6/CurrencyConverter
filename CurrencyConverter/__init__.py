ca51c32b82d969b360fae1adf2fc33b56f4580a5
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-08-20 21:35:42 -0400

    Update __init__.py

--- CurrencyConverter/__init__.py
+++ CurrencyConverter/__init__.py
@@ -1,10 +1,4 @@
-import asyncio
-from .CurrencyConverter import CurrencyConverter
+from .currencyconverter import CurrencyConverter
 
-__author__ = "Your name or username"
-__version__ = "1.0.0"
-__description__ = "A cog that converts currencies using API Ninjas"
-
-@asyncio.coroutine
 def setup(bot):
     bot.add_cog(CurrencyConverter(bot))
