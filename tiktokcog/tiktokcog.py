81d694a43a2613652378beed0281f0767ae93b1d
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-09-04 01:58:17 -0400

    Update tiktokcog.py

--- tiktokcog/tiktokcog.py
+++ tiktokcog/tiktokcog.py
@@ -1,30 +1,27 @@
 import re
 from redbot.core import commands
-from discord import Embed, utils
 
 class TikTokCog(commands.Cog):
     """A custom cog that reposts tiktok urls"""
 
     def __init__(self, bot):
         self.bot = bot
+        # Compile the tiktok pattern only once
         self.tiktok_pattern = re.compile(r"(?i)(https?://)?((\w+)\.)?tiktok.com/(.+)")
 
     @commands.Cog.listener()
     async def on_message(self, message):
         """A listener that triggers when a message is sent"""
         # Check if the message is from a user and contains a tiktok url
-        if not message.author.bot and self.tiktok_pattern.search(message.content):
-            # Extract the tiktok url from the message
-            tiktok_url = self.tiktok_pattern.search(message.content)
-            if tiktok_url:
-                tiktok_url = tiktok_url.group(0)
-                # Add vx in front of tiktok.com in the url, while preserving the protocol, subdomain, and path parts
-                new_url = re.sub(self.tiktok_pattern, r"\1\2vxtiktok.com/\4", tiktok_url)
-                # Get the discord mention of the person who posted the tiktok url
-                mention = message.author.mention
-                # Create a formatted message with the mention and modified url
-                formatted_message = f"{mention} originally shared this embedded TikTok video.\n{new_url}"
-                # Repost the formatted message
-                await message.channel.send(formatted_message)
-                # Remove the original message
-                await message.delete()
+        if message.author.bot:
+            return
+        tiktok_url = self.tiktok_pattern.search(message.content)
+        if not tiktok_url:
+            return
+        # Add vx in front of tiktok.com in the url, while preserving the protocol, subdomain, and path parts
+        new_url = tiktok_url.expand(r"\1\2vxtiktok.com/\4")
+        # Create a formatted message with the mention and modified url
+        formatted_message = f"{message.author.mention} originally shared this embedded TikTok video.\n{new_url}"
+        # Repost the formatted message and remove the original message
+        await message.channel.send(formatted_message)
+        await message.delete()
