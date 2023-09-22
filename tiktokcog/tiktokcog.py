78251f02cb14254e65bc0694cd561b664c7a0a1d
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-09-21 20:37:05 -0400

    Update tiktokcog.py

--- tiktokcog/tiktokcog.py
+++ tiktokcog/tiktokcog.py
@@ -24,8 +24,8 @@ class TikTokCog(commands.Cog):
         # Add vx in front of tiktok.com in the url, while preserving the protocol, subdomain, and path parts
         new_url = tiktok_url.expand(r"\1\2vxtiktok.com/\4")
         # Download the file from the user's avatar url using requests
-        # Change the size argument to a lower value, such as 256 or 128
-        response = requests.get(message.author.avatar.with_size(128))
+        # Change the size argument to a lower value
+        response = requests.get(message.author.avatar.with_size(32))
         # Create a BytesIO object from the response content using io
         file_data = io.BytesIO(response.content)
         # Create a file object from the BytesIO object using discord.File
