5c27f429d54d1a3b56f32a296745cfe9fd030322
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-09-22 22:51:12 -0400

    Update hellotest.py

--- hellotest/hellotest.py
+++ hellotest/hellotest.py
@@ -5,14 +5,15 @@ class HelloTest(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def hellotest(self, ctx):
-        """Creates a custom emoji out of the user's avatar and posts it."""
+@commands.command()
+async def hellotest(self, ctx):
+    """Creates a custom emoji out of the user's avatar and posts it."""
 
-        avatar_url = ctx.author.avatar_url
-        emoji = await ctx.guild.create_custom_emoji(avatar_url, name=ctx.author.name)
-        await ctx.send(f"Hello, {ctx.author.name}! Here is your custom emoji: {emoji}")
-        await emoji.delete()
+    avatar = ctx.author.avatar
+    avatar_url = avatar.url
+    emoji = await ctx.guild.create_custom_emoji(avatar_url, name=ctx.author.name)
+    await ctx.send(f"Hello, {ctx.author.name}! Here is your custom emoji: {emoji}")
+    await emoji.delete()
 
 async def setup(bot):
     await bot.add_cog(HelloTestCog(bot))
