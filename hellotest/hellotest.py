68024edd37bedeb3007200ff073be7430c9d54e7
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-09-22 22:24:05 -0400

    Add files via upload

---
+++ hellotest/hellotest.py
@@ -0,0 +1,30 @@
+from redbot.core import commands
+import discord
+import aiohttp
+
+class HelloTest(commands.Cog):
+    """A cog that creates a custom emoji with the user's avatar"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hellotest(self, ctx):
+        """This creates a custom emoji with your avatar and posts it"""
+        # Get the user's avatar URL
+        avatar_url = str(ctx.author.avatar_url_as(format="png"))
+
+        # Create a temporary session to download the avatar image
+        async with aiohttp.ClientSession() as session:
+            async with session.get(avatar_url) as response:
+                # Read the image data as bytes
+                image_data = await response.read()
+
+        # Create a custom emoji with the name "hellotest" and the image data
+        emoji = await ctx.guild.create_custom_emoji(name="hellotest", image=image_data)
+
+        # Send the emoji in the chat
+        await ctx.send(str(emoji))
+
+        # Delete the emoji from the server
+        await emoji.delete()
