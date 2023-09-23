751abf24f2b0b631d5c533420220cf7cf7787a9e
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-09-22 22:32:45 -0400

    Update hellotest.py

--- hellotest/hellotest.py
+++ hellotest/hellotest.py
@@ -11,17 +11,10 @@ class HelloTest(commands.Cog):
     @commands.command()
     async def hellotest(self, ctx):
         """This creates a custom emoji with your avatar and posts it"""
-        # Get the user's avatar URL
-        # Replace this line
-        # avatar_url = str(ctx.author.avatar_url_as(format="png"))
-        # With this line
-        avatar_url = str(ctx.author.avatar.url)
-
-        # Create a temporary session to download the avatar image
-        async with aiohttp.ClientSession() as session:
-            async with session.get(avatar_url) as response:
-                # Read the image data as bytes
-                image_data = await response.read()
+        # Get the user's avatar URL with a smaller size
+        avatar_url = ctx.author.avatar_url_as(size=128)
+        # Read the image data as bytes
+        image_data = await avatar_url.read()
 
         # Create a custom emoji with the name "hellotest" and the image data
         emoji = await ctx.guild.create_custom_emoji(name="hellotest", image=image_data)
