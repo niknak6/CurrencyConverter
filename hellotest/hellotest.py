a010f63825e00f8695d0188669f9825353a7a3f9
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-09-24 23:28:17 -0400

    Update hellotest.py

--- hellotest/hellotest.py
+++ hellotest/hellotest.py
@@ -1,32 +1,44 @@
-# Import discord.py and commands from redbot.core
-import discord
-from redbot.core import commands
 import os
+import random
+import requests
+from redbot.core import commands
 
-# Create a class for your cog and inherit from commands.Cog
+# Create a class for the cog
 class HelloTest(commands.Cog):
-  # Define the __init__ method and pass the bot as an argument
-  def __init__(self, bot):
-    self.bot = bot
 
-  # Create a command decorator and name your command hellotest
-  @commands.command()
-  # Define the hellotest method and pass the context as an argument
-  async def hellotest(self, ctx):
-    # Get the author of the message that invoked the command
-    author = ctx.author
-    # Get the avatar URL of the author using the 'avatar' and 'url' attributes
-    avatar_url = author.avatar.url
-    # Use aiohttp to get the avatar image as bytes
-    async with self.bot.http_session.get(avatar_url) as response:
-      avatar_bytes = await response.read()
-    # Use discord.File to create a file object from the bytes
-    avatar_file = discord.File(avatar_bytes, filename="avatar.png")
-    # Use discord.Guild.create_custom_emoji to create a custom emoji from the file on the server
-    emoji = await ctx.guild.create_custom_emoji(name="hellotest", image=avatar_file.read(), reason="Hellotest command")
-    # Send the emoji as a message
-    await ctx.send(str(emoji))
-    # Use discord.Guild.delete_custom_emoji to delete the custom emoji from the server
-    await ctx.guild.delete_custom_emoji(emoji, reason="Hellotest command")
-    # Remove the avatar.png file from your cogs folder
-    os.remove("cogs/avatar.png")
+    # Initialize the cog with the bot object
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Create a command called hellotest
+    @commands.command()
+    async def hellotest(self, ctx):
+
+        # Get the user object from the context
+        user = ctx.author
+
+        # Get the avatar URL from the user object
+        avatar_url = user.avatar_url
+
+        # Download the image from the URL and save it as avatar.png
+        response = requests.get(avatar_url)
+        with open("avatar.png", "wb") as file:
+            file.write(response.content)
+
+        # Get the guild object from the context
+        guild = ctx.guild
+
+        # Open the avatar image file in binary mode
+        with open("avatar.png", "rb") as image:
+
+            # Create a custom emoji with the name "user_avatar" and the image file
+            emoji = await guild.create_custom_emoji(name="user_avatar", image=image.read())
+
+        # Send a message with the custom emoji
+        await ctx.send(f"Here is your custom emoji: <:{emoji.name}:{emoji.id}>")
+
+        # Delete the custom emoji
+        await emoji.delete()
+
+        # Delete the avatar.png file
+        os.remove("avatar.png")
