d29d9ae6b8d959d2ce8a9307816996bae0979fbe
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-09-24 22:50:57 -0400

    Update hellotest.py

--- hellotest/hellotest.py
+++ hellotest/hellotest.py
@@ -12,21 +12,21 @@ class HelloTest(commands.Cog):
   # Create a command decorator and name your command hellotest
   @commands.command()
   # Define the hellotest method and pass the context as an argument
-  async def hellotest(self, ctx):
-    # Get the author of the message that invoked the command
-    author = ctx.author
-    # Get the avatar URL of the author
-    avatar_url = author.avatar_url
-    # Use aiohttp to get the avatar image as bytes
-    async with self.bot.session.get(avatar_url) as response:
-      avatar_bytes = await response.read()
-    # Use discord.File to create a file object from the bytes
-    avatar_file = discord.File(avatar_bytes, filename="avatar.png")
-    # Use discord.Guild.create_custom_emoji to create a custom emoji from the file on the server
-    emoji = await ctx.guild.create_custom_emoji(name="hellotest", image=avatar_file.read(), reason="Hellotest command")
-    # Send the emoji as a message
-    await ctx.send(str(emoji))
-    # Use discord.Guild.delete_custom_emoji to delete the custom emoji from the server
-    await ctx.guild.delete_custom_emoji(emoji, reason="Hellotest command")
-    # Remove the avatar.png file from your cogs folder
-    os.remove("cogs/avatar.png")
+async def hellotest(self, ctx):
+  # Get the author of the message that invoked the command
+  author = ctx.author
+  # Get the avatar URL of the author using the 'avatar' and 'url' attributes
+  avatar_url = author.avatar.url
+  # Use aiohttp to get the avatar image as bytes
+  async with self.bot.session.get(avatar_url) as response:
+    avatar_bytes = await response.read()
+  # Use discord.File to create a file object from the bytes
+  avatar_file = discord.File(avatar_bytes, filename="avatar.png")
+  # Use discord.Guild.create_custom_emoji to create a custom emoji from the file on the server
+  emoji = await ctx.guild.create_custom_emoji(name="hellotest", image=avatar_file.read(), reason="Hellotest command")
+  # Send the emoji as a message
+  await ctx.send(str(emoji))
+  # Use discord.Guild.delete_custom_emoji to delete the custom emoji from the server
+  await ctx.guild.delete_custom_emoji(emoji, reason="Hellotest command")
+  # Remove the avatar.png file from your cogs folder
+  os.remove("cogs/avatar.png")
