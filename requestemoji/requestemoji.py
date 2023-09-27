02820b01c690b8f060f6f8c30fbb72ac61b0b52e
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-09-27 15:59:32 -0400

    Update requestemoji.py

--- requestemoji/requestemoji.py
+++ requestemoji/requestemoji.py
@@ -1,203 +1,197 @@
 # Import the necessary modules
 import discord
-from redbot.core import commands
+from redbot.core import commands, checks, Config
 from redbot.core.utils.chat_formatting import humanize_list
 from PIL import Image
 import io
 
 # Define a class for the cog
-class RequestEmoji (commands.Cog):
-  # Initialize the cog with the bot instance
-  def __init__ (self, bot):
-    self.bot = bot
-    # You can also initialize a database or a config object here to store the requests and approvals
+class RequestEmoji(commands.Cog):
+    """A cog for requesting custom stickers and emojis."""
 
-  # Define a command group for requesting stickers or emojis
-  @commands.group ()
-  async def request (self, ctx):
-    # If no subcommand is invoked, show the help message
-    if ctx.invoked_subcommand is None:
-      await ctx.send_help ()
+    # Initialize the cog with the bot instance and a Config object
+    def __init__(self, bot):
+        self.bot = bot
+        # Create a Config object to store the requests and approvals
+        self.config = Config.get_conf(self, identifier=1234567890)
+        # Register a guild-level config with default values
+        default_guild = {"requests": {}, "approvals": {}}
+        self.config.register_guild(**default_guild)
 
-  # Define a subcommand for requesting stickers
-  @request.command ()
-  async def sticker (self, ctx, name: str):
-    # Check if the name is valid (at least 2 characters)
-    if len (name) < 2:
-      await ctx.send ("The sticker name must be at least 2 characters.")
-      return
+    # Define a command group for requesting stickers or emojis
+    @commands.group()
+    async def request(self, ctx):
+        """Request a custom sticker or emoji."""
+        # If no subcommand is invoked, show the help message
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help()
 
-    # Check if there is an attachment in the message
-    if not ctx.message.attachments:
-      await ctx.send ("You must attach an image file for the sticker.")
-      return
+    # Define a subcommand for requesting stickers
+    @request.command()
+    @commands.cooldown(1, 60, commands.BucketType.user)  # Limit to one request per minute per user
+    async def sticker(self, ctx, name: str):
+        """Request a sticker with a given name."""
+        # Check if the name is valid (at least 2 characters)
+        if len(name) < 2:
+            await ctx.send("The sticker name must be at least 2 characters.")
+            return
 
-    # Get the attachment and check if it is an image file
-    attachment = ctx.message.attachments [0]
-    if not attachment.content_type.startswith ("image/"):
-      await ctx.send ("The attachment must be an image file.")
-      return
+        # Check if there is an attachment in the message
+        if not ctx.message.attachments:
+            await ctx.send("You must attach an image file for the sticker.")
+            return
 
-    # Download the attachment as bytes
-    image_bytes = await attachment.read ()
+        # Get the attachment and check if it is an image file
+        attachment = ctx.message.attachments[0]
+        if not attachment.content_type.startswith("image/"):
+            await ctx.send("The attachment must be an image file.")
+            return
 
-    # Open the image with PIL and check its size and format
-    image = Image.open (io.BytesIO(image_bytes))
-    width, height = image.size
-    format = image.format
+        # Download the attachment as bytes
+        image_bytes = await attachment.read()
 
-    # Check if the image size is exactly 320x320
-    if width != 320 or height != 320:
-      # Resize the image using Image.thumbnail method with size (320, 320)
-      # This will preserve the aspect ratio and not exceed the original size or the given size
-      image.thumbnail ((320, 320))
+        # Open the image with PIL and check its size and format
+        image = Image.open(io.BytesIO(image_bytes))
+        width, height = image.size
+        format = image.format
 
-    # Check if the image format is PNG or APNG
-    if format not in ["PNG", "APNG"]:
-      # Convert the image format to PNG
-      image = image.convert ("RGBA")
+        # Check if the image size is exactly 320x320
+        if width != 320 or height != 320:
+            # Resize the image using Image.thumbnail method with size (320, 320)
+            # This will preserve the aspect ratio and not exceed the original size or the given size
+            image.thumbnail((320, 320))
 
-    # Save the image as bytes again
-    image_bytes = io.BytesIO ()
-    image.save (image_bytes, format="PNG")
-    image_bytes.seek (0)
+        # Check if the image format is PNG or APNG
+        if format not in ["PNG", "APNG"]:
+            # Convert the image format to PNG
+            image = image.convert("RGBA")
 
-    # Create a discord.File object from the bytes and the filename
-    file = discord.File (file_data, filename=attachment.filename)
+        # Save the image as bytes again
+        image_bytes = io.BytesIO()
+        image.save(image_bytes, format="PNG")
+        image_bytes.seek(0)
 
-    # Send a confirmation message with the sticker preview and reactions
-    message = await ctx.send (
-      f"{ctx.author.mention} has requested a sticker with name {name}. Here is a preview:",
-      file=file,
-    )
-    await message.add_reaction ("\u2705") # Checkmark emoji
-    await message.add_reaction ("\u274c") # Cross emoji
+        # Create a discord.File object from the bytes and the filename
+        file = discord.File(image_bytes, filename="sticker.png")
 
-    # You can also store the request information in a database or a config object here
+        # Send a confirmation message with the sticker preview and reactions
+        message = await ctx.send(
+            f"{ctx.author.mention} has requested a sticker with name {name}. Here is a preview:",
+            file=file,
+        )
+        await message.add_reaction("\u2705")  # Checkmark emoji
+        await message.add_reaction("\u274c")  # Cross emoji
 
-  # Define a subcommand for requesting emojis
-  @request.command ()
-  async def emoji (self, ctx, name: str):
-    # Check if the name is valid (at least 2 characters)
-    if len (name) < 2:
-      await ctx.send ("The emoji name must be at least 2 characters.")
-      return
+        # Store the request information in the config object
+        await self.config.guild(ctx.guild).requests.set_raw(
+            message.id, value={"name": name, "file": file}
+        )
 
-    # Check if there is an attachment in the message
-    if not ctx.message.attachments:
-      await ctx.send ("You must attach an image file for the emoji.")
-      return
+    # Define a subcommand for requesting emojis
+    @request.command()
+    @commands.cooldown(1, 60, commands.BucketType.user)  # Limit to one request per minute per user
+    async def emoji(self, ctx, name: str):
+        """Request an emoji with a given name."""
+        # Check if the name is valid (at least 2 characters)
+        if len(name) < 2:
+            await ctx.send("The emoji name must be at least 2 characters.")
+            return
 
-    # Get the attachment and check if it is an image file
-    attachment = ctx.message.attachments [0]
-    if not attachment.content_type.startswith ("image/"):
-      await ctx.send ("The attachment must be an image file.")
-      return
+        # Check if there is an attachment in the message
+        if not ctx.message.attachments:
+            await ctx.send("You must attach an image file for the emoji.")
+            return
 
-    # Download the attachment as bytes
-    image_bytes = await attachment.read ()
+        # Get the attachment and check if it is an image file
+        attachment = ctx.message.attachments[0]
+        if not attachment.content_type.startswith("image/"):
+            await ctx.send("The attachment must be an image file.")
+            return
 
-    # Open the image with PIL and check its size and format
-    image = Image.open (io.BytesIO(image_bytes))
-    width, height = image.size
-    format = image.format
+        # Download the attachment as bytes
+        image_bytes = await attachment.read()
 
-    # Check if the image size is between 32x32 and 128x128
-    if width < 32 or height < 32 or width > 128 or height > 128:
-      # Resize the image using Image.thumbnail method with size (128, 128)
-      # This will preserve the aspect ratio and not exceed the original size or the given size
-      image.thumbnail ((128, 128))
+        # Open the image with PIL and check its size and format
+        image = Image.open(io.BytesIO(image_bytes))
+        width, height = image.size
+        format = image.format
 
-    # Check if the image format is PNG or GIF
-    if format not in ["PNG", "GIF"]:
-      # Convert the image format to PNG
-      image = image.convert ("RGBA")
+        # Check if the image size is between 32x32 and 128x128
+        if width < 32 or height < 32 or width > 128 or height > 128:
+            # Resize the image using Image.thumbnail method with size (128, 128)
+            # This will preserve the aspect ratio and not exceed the original size or the given size
+            image.thumbnail((128, 128))
 
-    # Save the image as bytes again
-    image_bytes = io.BytesIO ()
-    image.save (image_bytes, format="PNG")
-    image_bytes.seek (0)
+        # Check if the image format is PNG or GIF
+        if format not in ["PNG", "GIF"]:
+            # Convert the image format to PNG
+            image = image.convert("RGBA")
 
-    # Create a discord.File object from the bytes and the filename
-    file = discord.File (file_data, filename=attachment.filename)
+        # Save the image as bytes again
+        image_bytes = io.BytesIO()
+        image.save(image_bytes, format="PNG")
+        image_bytes.seek(0)
 
-    # Send a confirmation message with the emoji preview and reactions
-    message = await ctx.send (
-      f"{ctx.author.mention} has requested an emoji with name {name}. Here is a preview:",
-      file=file,
-    )
-    await message.add_reaction ("\u2705") # Checkmark emoji
-    await message.add_reaction ("\u274c") # Cross emoji
+        # Create a discord.File object from the bytes and the filename
+        file = discord.File(image_bytes, filename="emoji.png")
 
-    # You can also store the request information in a database or a config object here
+        # Send a confirmation message with the emoji preview and reactions
+        message = await ctx.send(
+            f"{ctx.author.mention} has requested an emoji with name {name}. Here is a preview:",
+            file=file,
+        )
+        await message.add_reaction("\u2705")  # Checkmark emoji
+        await message.add_reaction("\u274c")  # Cross emoji
 
-  # Define an event listener for reaction add
-  @commands.Cog.listener ()
-  async def on_raw_reaction_add (self, payload):
-    # Check if the reaction is on a message sent by the bot itself
-    if payload.user_id != self.bot.user.id:
-      # Get the guild, member, message, and emoji from the payload
-      guild = self.bot.get_guild (payload.guild_id)
-      member = guild.get_member (payload.user_id)
-      channel = guild.get_channel (payload.channel_id)
-      message = await channel.fetch_message (payload.message_id)
-      emoji = payload.emoji
+        # Store the request information in the config object
+        await self.config.guild(ctx.guild).requests.set_raw(
+            message.id, value={"name": name, "file": file}
+        )
 
-      # Check if the member has administrator permission
-      if member.guild_permissions.administrator:
-        # Check if the emoji is a checkmark or a cross
-        if emoji.name == "\u2705": # Checkmark emoji
-          # Approve the request
-          # Get the request information from the message or the database or the config object
-          # For simplicity, I will assume that the request information is in the message content and attachment
-          request_type, name = message.content.split ("with name")
-          request_type = request_type.strip ().split () [-1]
-          name = name.strip ()
-          attachment = message.attachments [0]
+    # Define an event listener for reaction add
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        """Handle approvals or denials of requests."""
+        # Check if the reaction is on a message sent by the bot itself
+        if payload.user_id != self.bot.user.id:
+            try:
+                # Get the guild, member, message, and emoji from the payload
+                guild = self.bot.get_guild(payload.guild_id)
+                member = guild.get_member(payload.user_id)
+                channel = guild.get_channel(payload.channel_id)
+                message = await channel.fetch_message(payload.message_id)
+                emoji = payload.emoji
 
-          # Create a custom sticker or emoji with the request information
-          if request_type == "sticker":
-            # Get the file data from the attachment as bytes
-            file_data = await attachment.read ()
-            # Create a discord.File object from the bytes and the filename
-            file = discord.File (file_data, filename=attachment.filename)
-            # Create a sticker using the file and other parameters
-            sticker = await guild.create_sticker (
-              name=name,
-              description="Requested by " + str (member),
-              emoji="\u2705", # Checkmark emoji as expression
-              file=file,
-              reason="Approved by " + str (member),
-            )
-            # Send a success message with the sticker
-            await channel.send (
-              f"{member.mention} has approved {message.author.mention}'s request for a sticker with name {name}. Here is the sticker: {sticker}"
-            )
-          elif request_type == "emoji":
-            # Get the file data from the attachment as bytes
-            file_data = await attachment.read ()
-            # Create a discord.File object from the bytes and the filename
-            file = discord.File (file_data, filename=attachment.filename)
-            # Create an emoji using the file and other parameters
-            emoji = await guild.create_custom_emoji (
-              name=name,
-              image=file_data,
-              reason="Approved by " + str (member),
-            )
-            # Send a success message with the emoji
-            await channel.send (
-              f"{member.mention} has approved {message.author.mention}'s request for an emoji with name {name}. Here is the emoji: {emoji}"
-            )
+                # Check if the member has administrator permission
+                if member.guild_permissions.administrator:
+                    # Check if the emoji is a checkmark or a cross
+                    if emoji.name == "\u2705":  # Checkmark emoji
+                        # Approve the request
+                        try:
+                            # Get the request information from the config object
+                            request_data = await self.config.guild(guild).requests.get_raw(
+                                message.id
+                            )
+                            name = request_data["name"]
+                            file = request_data["file"]
+                        except KeyError:
+                            return
 
-          # Delete the request message
-          await message.delete ()
-
-        elif emoji.name == "\u274c": # Cross emoji
-          # Deny the request
-          # Send a failure message with the reason
-          await channel.send (
-            f"{member.mention} has denied {message.author.mention}'s request for a {request_type} with name {name}."
-          )
-
-          # Delete the request message
-          await message.delete ()
+                        # Create a custom sticker or emoji with the request information
+                        if file.filename == "sticker.png":
+                            sticker = await guild.create_sticker(
+                                name=name,
+                                description=f"Requested by {member}",
+                                emoji="\u2705",  # Checkmark emoji as expression
+                                file=file,
+                                reason=f"Approved by {member}",
+                            )
+                            # Send a success message with the sticker
+                            embed = discord.Embed(
+                                title=f"Sticker approved",
+                                description=f"{member.mention} has approved {message.author.mention}'s request for a sticker with name {name}.",
+                                color=discord.Color.green(),
+                            )
+                            embed.set_image(url=sticker.url)
+                            await channel.send(embed=embed)
+                        elif file
