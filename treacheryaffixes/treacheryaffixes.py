1c541916b9dc84855ab7059878f4b9fda2e8a995
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-06 11:38:24 -0400

    Update treacheryaffixes.py

--- treacheryaffixes/treacheryaffixes.py
+++ treacheryaffixes/treacheryaffixes.py
@@ -9,53 +9,26 @@ import re
 
 # Define a function to scrape the data from the website
 def scrape_data(url):
-    # Send a GET request to the website and store the response
     response = requests.get(url)
-
-    # Check if the response status code is 200 (OK)
     if response.status_code == 200:
-        # Parse the response content as HTML using BeautifulSoup
         soup = BeautifulSoup(response.content, "html.parser")
-
-        # Find the table element that contains the affixes data
         table = soup.find("table", class_="affixes_overview_table")
-
-        # Check if the table element exists
         if table:
-            # Create an empty list to store the affixes data
             affixes_data = []
-
-            # Loop through each row of the table body
             for row in table.find("tbody").find_all("tr"):
-                # Create an empty dictionary to store the row data
                 row_data = {}
-
-                # Get the start date of the week from the first column
                 start_date = row.find("td", class_="first_column").get_text().strip()
                 row_data["start_date"] = start_date
-
-                # Get the affix names from the other columns
                 affix_names = []
                 for affix in row.find_all("td")[1:]:
-                    # Get the text of the affix and remove any trailing pipe or whitespace characters
                     affix_name = affix.get_text().strip().rstrip("| ")
-                    # Append the cleaned affix name to the list
                     affix_names.append(affix_name)
-
-                # Slice the list to only include the first three elements
                 affix_names = affix_names[:3]
-
                 row_data["affix_names"] = affix_names
-
-                # Append the row data to the affixes data list
                 affixes_data.append(row_data)
-
-            # Return the affixes data list
             return affixes_data
-        
         else:
             return "Table element not found"
-    
     else:
         return f"Failed to fetch data from {url}"
 
@@ -70,7 +43,7 @@ def format_embed(data, title):
         date = row["start_date"]
         level2, level7, level14 = row["affix_names"]
         
-        date_pattern = re.compile(r"\d{4}/\w{3}/\d{2}\n\n\n     @ \d{2}h")
+        date_pattern = re.compile(r"\d{4}/\w{3}/\d{2}\n\n\n     @ %Hh")
         
         if date_pattern.match(date):
             date_obj = datetime.strptime(date, "%Y/%b/%d\n\n\n     @ %Hh")
@@ -116,11 +89,19 @@ class TreacheryAffixes(commands.Cog):
         current_week_embed = format_embed(current_week_data, "Current week")
         upcoming_weeks_embed = format_embed(upcoming_weeks_data, "Upcoming weeks")
 
-        await ctx.send(f"Current week embed: {current_week_embed}")
-        await ctx.send(f"Upcoming weeks embed: {upcoming_weeks_embed}")
+        if isinstance(current_week_embed, str):
+            await ctx.send(f"Current week embed: {current_week_embed}")
+        else:
+            await ctx.send(f"Current week embed: {current_week_embed.description}")
 
-        embed_message = discord.Embed(title="M+ Affixes from keystone.guru")
-        embed_message.add_field(name="Current week", value=current_week_embed.description)
-        embed_message.add_field(name="Upcoming weeks", value=upcoming_weeks_embed.description)
+        if isinstance(upcoming_weeks_embed, str):
+            await ctx.send(f"Upcoming weeks embed: {upcoming_weeks_embed}")
+        else:
+            await ctx.send(f"Upcoming weeks embed: {upcoming_weeks_embed.description}")
 
-        await ctx.send(embed=embed_message)
+        if not isinstance(current_week_embed, str) and not isinstance(upcoming_weeks_embed, str):
+            embed_message = discord.Embed(title="M+ Affixes from keystone.guru")
+            embed_message.add_field(name="Current week", value=current_week_embed.description)
+            embed_message.add_field(name="Upcoming weeks", value=upcoming_weeks_embed.description)
+
+            await ctx.send(embed=embed_message)
