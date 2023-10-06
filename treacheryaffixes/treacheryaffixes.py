502aec7af82c7999a08dc562ee4bfe6266cf4bf7
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-06 12:25:38 -0400

    Update treacheryaffixes.py

--- treacheryaffixes/treacheryaffixes.py
+++ treacheryaffixes/treacheryaffixes.py
@@ -1,7 +1,123 @@
-Current week data: [{'start_date': '2023/Aug/01\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Incorporeal', 'Spiteful']}, {'start_date': '2023/Aug/08\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Afflicted', 'Raging']}, {'start_date': '2023/Aug/15\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Volcanic', 'Sanguine']}, {'start_date': '2023/Aug/22\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Storming', 'Bursting']}, {'start_date': '2023/Aug/29\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Afflicted', 'Bolstering']}, {'start_date': 'Legion timewalking', 'affix_names': ['Tyrannical', 'Bursting', 'Volcanic']}, {'start_date': '2023/Sep/05\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Incorporeal', 'Sanguine']}, {'start_date': '2023/Sep/12\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Entangling', 'Bursting']}, {'start_date': '2023/Sep/19\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Volcanic', 'Spiteful']}, {'start_date': '2023/Sep/26\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Storming', 'Raging']}, {'start_date': '2023/Oct/03\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Entangling', 'Bolstering']}]
-Upcoming weeks data: [{'start_date': '2023/Oct/10\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Incorporeal', 'Spiteful']}, {'start_date': '2023/Oct/17\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Afflicted', 'Raging']}, {'start_date': '2023/Oct/24\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Volcanic', 'Sanguine']}, {'start_date': '2023/Oct/31\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Storming', 'Bursting']}, {'start_date': '2023/Nov/07\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Afflicted', 'Bolstering']}, {'start_date': '2023/Nov/14\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Incorporeal', 'Sanguine']}, {'start_date': '2023/Nov/21\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Entangling', 'Bursting']}, {'start_date': '2023/Nov/28\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Volcanic', 'Spiteful']}, {'start_date': '2023/Dec/05\n\n\n     @ 15h', 'affix_names': ['Tyrannical', 'Storming', 'Raging']}, {'start_date': '2023/Dec/12\n\n\n     @ 15h', 'affix_names': ['Fortified', 'Entangling', 'Bolstering']}]
-Current week embed:
-Upcoming weeks embed:
-M+ Affixes from keystone.guru
-Current week
-Upcoming weeks
+# Import the necessary modules
+import discord
+from redbot.core import commands
+from redbot.core.utils.chat_formatting import bold
+from datetime import datetime, timedelta
+import requests
+from bs4 import BeautifulSoup
+import re
+
+# Define a function to scrape the data from the website
+def scrape_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        table = soup.find("table", class_="affixes_overview_table")
+        if table:
+            affixes_data = []
+            for row in table.find("tbody").find_all("tr"):
+                row_data = {}
+                start_date = row.find("td", class_="first_column").get_text().strip()
+                row_data["start_date"] = start_date
+                affix_names = []
+                for affix in row.find_all("td")[1:]:
+                    affix_name = affix.get_text().strip().rstrip("| ")
+                    affix_names.append(affix_name)
+                affix_names = affix_names[:3]
+                row_data["affix_names"] = affix_names
+                affixes_data.append(row_data)
+            return affixes_data
+        else:
+            return "Table element not found"
+    else:
+        return f"Failed to fetch data from {url}"
+
+# Define a function to format the data as an embed message
+def format_embed(data, title):
+    if isinstance(data, str):
+        return data
+
+    embed_description = ""
+    
+    for row in data:
+        date = row["start_date"]
+        level2, level7, level14 = row["affix_names"]
+        
+        # Check if the date string contains digits
+        if any(char.isdigit() for char in date):
+            date_pattern = re.compile(r"\d{4}/\w{3}/\d{2}.*")
+            
+            if date_pattern.match(date):
+                date_obj = datetime.strptime(date[:15], "%Y/%b/%d\n\n\n")
+            
+                date_str = date_obj.strftime("%m/%d/%y")
+            
+                today = datetime.today()
+                start = today - timedelta(days=(today.weekday() - 1) % 7)
+                
+                if today.weekday() < 1:
+                    start -= timedelta(days=7)
+                
+                end = start + timedelta(days=6)
+            
+                if start <= date_obj <= end:
+                    bs = "\\"
+                    embed_description += f"**__{date_str}__**\n{level2} | {level7} | {level14.rstrip(bs)}\n"
+            
+            else:
+                continue
+        
+        else:
+            continue
+    
+    embed_message = discord.Embed(title=title, description=embed_description) 
+    return embed_message 
+
+class TreacheryAffixes(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def debugaffixes(self, ctx):
+        urls = ["https://keystone.guru/affixes", "https://keystone.guru/affixes?offset=1"]
+
+        current_week_data = scrape_data("https://keystone.guru/affixes")
+        upcoming_weeks_data = scrape_data("https://keystone.guru/affixes?offset=1")
+
+        await ctx.send(f"Current week data: {current_week_data}")
+        await ctx.send(f"Upcoming weeks data: {upcoming_weeks_data}")
+
+        current_week_embed = format_embed(current_week_data, "Current week")
+        upcoming_weeks_embed = format_embed(upcoming_weeks_data, "Upcoming weeks")
+
+        if isinstance(current_week_embed, str):
+            await ctx.send(f"Current week embed: {current_week_embed}")
+        else:
+            await ctx.send(f"Current week embed: {current_week_embed.description}")
+
+        if isinstance(upcoming_weeks_embed, str):
+            await ctx.send(f"Upcoming weeks embed: {upcoming_weeks_embed}")
+        else:
+            await ctx.send(f"Upcoming weeks embed: {upcoming_weeks_embed.description}")
+
+        if not isinstance(current_week_embed, str) and not isinstance(upcoming_weeks_embed, str):
+            embed_message = discord.Embed(title="M+ Affixes from keystone.guru")
+            embed_message.add_field(name="Current week", value=current_week_embed.description)
+            embed_message.add_field(name="Upcoming weeks", value=upcoming_weeks_embed.description)
+
+            await ctx.send(embed=embed_message)
+
+    @commands.command()
+    async def affixes(self, ctx):
+        urls = ["https://keystone.guru/affixes", "https://keystone.guru/affixes?offset=1"]
+
+        current_week_embed = format_embed(scrape_data("https://keystone.guru/affixes"), "Current week")
+        upcoming_weeks_embed = format_embed(scrape_data("https://keystone.guru/affixes?offset=1"), "Upcoming weeks")
+
+        if not isinstance(current_week_embed, str) and not isinstance(upcoming_weeks_embed, str):
+            embed_message = discord.Embed(title="M+ Affixes from keystone.guru")
+            embed_message.add_field(name="Current week", value=current_week_embed.description)
+            embed_message.add_field(name="Upcoming weeks", value=upcoming_weeks_embed.description)
+
+            await ctx.send(embed=embed_message)
