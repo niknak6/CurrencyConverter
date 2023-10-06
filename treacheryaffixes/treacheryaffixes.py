4ed925f37d98941ca277994fb7098c0375c9d386
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-06 11:12:46 -0400

    Update treacheryaffixes.py

--- treacheryaffixes/treacheryaffixes.py
+++ treacheryaffixes/treacheryaffixes.py
@@ -83,53 +83,39 @@ def format_embed(data, title):
             # Convert the date to US standard of MM/DD/YY and drop the time
             date_str = date_obj.strftime("%m/%d/%y")
         
-            # Check if the date is within or after the current week
+            # Check if the date is within or after this week or last week (if today is Monday)
             today = datetime.today()
-            start = today - timedelta(days=(today.weekday() - 1) % 7) # Tuesday is weekday 1
+            start = today - timedelta(days=(today.weekday() - 1) % 7)  # Tuesday is weekday 1
+            
+            if today.weekday() < 1:  # If today is Monday (weekday 0), adjust start to last Tuesday
+                start -= timedelta(days=7)
+            
             end = start + timedelta(days=6)
         
-            # If yes, bold and underline the row and add a newline at the end
-            if start <= date_obj:
-                # Define a variable for the backslash character
+            if start <= date_obj <= end:
                 bs = "\\"
-
-                # Use the variable outside of the curly braces of the f-string
-                embed_description += f"**__{date_str}__**\n{level2} | {level7} | {level14.rstrip(bs)}\n" 
+                embed_description += f"**__{date_str}__**\n{level2} | {level7} | {level14.rstrip(bs)}\n"
         
-            # If no, skip it or handle it differently 
             else:
                 continue
         
         else:
-            # Skip it or handle it differently 
             continue
     
-    # Create an embed message object with a title and a description using discord.Embed()
     embed_message = discord.Embed(title=title, description=embed_description) 
-    
-    # Return the embed message object 
     return embed_message 
 
-# Define a class for the cog
 class TreacheryAffixes(commands.Cog):
     
-    # Define the constructor
     def __init__(self, bot):
         self.bot = bot
     
-    # Define a command for affixes
     @commands.command()
     async def affixes(self, ctx):
-        # Define a list of urls of the website
         urls = ["https://keystone.guru/affixes", "https://keystone.guru/affixes?offset=1"]
 
-        # Create one embed message with two fields, one for the current week and one for the upcoming weeks
         embed_message = discord.Embed(title="M+ Affixes from keystone.guru")
         embed_message.add_field(name="Current week", value=format_embed(scrape_data("https://keystone.guru/affixes"), "Current week").description)
         embed_message.add_field(name="Upcoming weeks", value=format_embed(scrape_data("https://keystone.guru/affixes?offset=1"), "Upcoming weeks").description)
 
-        # Remove the emojis from the embed message
-        # embed_message.set_footer(text=":fire: :zap: :snowflake: :skull:")
-
-        # Send the embed message to the channel
         await ctx.send(embed=embed_message)
