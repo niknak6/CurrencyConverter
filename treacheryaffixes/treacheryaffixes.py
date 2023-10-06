050343d45df4263355389e26621146c54a2b6372
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-06 13:18:34 -0400

    Update treacheryaffixes.py

--- treacheryaffixes/treacheryaffixes.py
+++ treacheryaffixes/treacheryaffixes.py
@@ -111,4 +111,16 @@ class TreacheryAffixes(commands.Cog):
 
             await ctx.send(embed=embed_message)
 
-    @commands.command
+    @commands.command()
+    async def affixes(self, ctx):
+        urls = ["https://keystone.guru/affixes", "https://keystone.guru/affixes?offset=1"]
+
+        current_week_embed = format_embed(scrape_data("https://keystone.guru/affixes"), "Current week", upcoming_weeks=1)
+        upcoming_weeks_embed = format_embed(scrape_data("https://keystone.guru/affixes?offset=1"), "Upcoming weeks")
+
+        if not isinstance(current_week_embed, str) and not isinstance(upcoming_weeks_embed, str):
+            embed_message = discord.Embed(title="M+ Affixes from keystone.guru")
+            embed_message.add_field(name="Current week", value=current_week_embed.description)
+            embed_message.add_field(name="Upcoming weeks", value=upcoming_weeks_embed.description)
+
+            await ctx.send(embed=embed_message)
