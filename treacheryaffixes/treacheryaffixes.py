098486d340b261d6641abaa84b37ba7ff4b3130b
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-10 21:49:12 -0400

    Update treacheryaffixes.py

--- treacheryaffixes/treacheryaffixes.py
+++ treacheryaffixes/treacheryaffixes.py
@@ -6,29 +6,8 @@ class TreacheryAffixes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def affixes(self, ctx):
-        # Fetch the HTML from the website
-        response = requests.get('https://keystone.guru/affixes')
-        response.raise_for_status()
-
-        # Parse the HTML
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Find the table row for the current week
-        current_week_row = soup.find_all('tr', {'class': ['table_row even', 'current_week']})[-1]
-
-        # Find the date in the first column
-        date = current_week_row.find('td', {'class': 'first_column'}).get_text(strip=True)
-
-        # Find the affixes in the other columns
-        affixes = [td.get_text(strip=True) for td in current_week_row.find_all('td')[1:-1]]
-
-        # Send a message with the affixes
-        await ctx.send(f"Date: {date}\nAffixes: {', '.join(affixes)}")
-
-    @commands.command()
-    async def future_affixes(self, ctx, offset: int):
+    @staticmethod
+    def fetch_and_parse_affixes(offset: int):
         # Fetch the HTML from the website
         response = requests.get(f'https://keystone.guru/affixes?offset={offset}')
         response.raise_for_status()
@@ -52,5 +31,14 @@ class TreacheryAffixes(commands.Cog):
             # Append the date and affixes to the string
             all_affixes += f"Date: {date}\nAffixes: {', '.join(affixes)}\n\n"
 
-        # Send a message with all affixes
-        await ctx.send(all_affixes)
+        return all_affixes
+
+    @commands.command()
+    async def affixes(self, ctx):
+        affixes = self.fetch_and_parse_affixes(0)
+        await ctx.send(affixes)
+
+    @commands.command()
+    async def future_affixes(self, ctx, offset: int):
+        affixes = self.fetch_and_parse_affixes(offset)
+        await ctx.send(affixes)
