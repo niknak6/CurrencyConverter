e4e1384b2c723f327317d4ff688bf1b3c1da1f2b
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-14 04:36:49 -0400

    Update treacherypins.py

--- treacherypins/treacherypins.py
+++ treacherypins/treacherypins.py
@@ -12,13 +12,10 @@ class TreacheryPins(commands.Cog):
     @commands.command()
     async def pinboard(self, ctx):
         """Creates a new Treachery Pinboard and pins it to the current channel."""
-        try:
-            message = await ctx.send(f"__**{ctx.channel.name} Pinboard**__") # Send a new message with the channel name as the content
-            await message.pin() # Pin the message to the current channel
-            self.pinboards[ctx.channel.id] = message.id # Store the pinboard message ID in the dictionary
-            await ctx.send("A new pinboard has been created and pinned.")
-        except Exception as e:
-            await ctx.send(f"An error occurred: {e}")
+        message = await ctx.send(f"__**{ctx.channel.name} Pinboard**__") # Send a new message with the channel name as the content
+        await message.pin() # Pin the message to the current channel
+        self.pinboards[ctx.channel.id] = message.id # Store the pinboard message ID in the dictionary
+        await ctx.send("A new pinboard has been created and pinned.")
 
     @commands.command()
     async def removepinboard(self, ctx):
@@ -43,45 +40,41 @@ class TreacheryPins(commands.Cog):
             if payload.channel_id in self.pinboards: # Check if the channel has a pinboard
                 channel = self.bot.get_channel(payload.channel_id) # Get the channel object
                 user = self.bot.get_user(payload.user_id) # Get the user object
-                await channel.send(f"{user.mention}, please reply with the subject of the message you want to pin.") # Prompt the user for the message subject
-                message_to_pin = payload.message_id # Store the message ID that received the push pin reaction in a local variable
-
+                
                 def check_message(message):
                     """A function that checks if
 
 the message is a valid response to the prompt."""
-                    return message.author != self.bot.user and message.channel.id in self.pinboards and message_to_pin == message.id # Return True if the message is not from the bot, is in a channel with a pinboard, and matches the message ID that received the push pin reaction
+                    return message.author != self.bot.user and message.channel.id in self.pinboards and payload.message_id == message.id # Return True if the message is not from the bot, is in a channel with a pinboard, and matches the message ID that received the push pin reaction
 
-                await self.on_message(await self.bot.wait_for("message", check=check_message), message_to_pin) # Wait for a message that passes the check function and pass it along with the local variable to the on_message listener
+                await self.on_message(await self.bot.wait_for("message", check=check_message), payload.message_id) # Wait for a message that passes the check function and pass it along with the local variable to the on_message listener
 
     @commands.Cog.listener()
-    async def on_message(self, message, message_to_pin): # MODIFIED
+    async def on_message(self, message, message_to_pin): 
         """Handles
 
 the message events."""
-        # nonlocal message_to_pin # Use the nonlocal keyword to access the local variable from the outer function # MODIFIED - REMOVED
-        if not hasattr(self, "message_to_pin"): # Check if there is a message ID stored in self.message_to_pin 
-            return # If not, return early 
-        if message.author == self.bot.user: # Ignore messages from the bot
+        if not hasattr(self, "message_to_pin"): 
+            return 
+        if message.author == self.bot.user: 
             return
-        if message.channel.id in self.pinboards: # Check if
+        if message.channel.id in self.pinboards: 
 
             
-            pinboard_id = self.pinboards[message.channel.id] # Get the pinboard message ID
-            pinboard = await message.channel.fetch_message(pinboard_id) # Fetch the pinboard message object
-            embeds = pinboard.embeds # Get the list of embeds in the pinboard message
-            message_to_pin = await message.channel.fetch_message(message_to_pin) # Fetch the message object that received the push pin reaction using the local variable
-            embed = Embed(title=message.content, url=message_to_pin.jump_url) # Create a new embed with the message subject and link to the message to be pinned
-            embeds.append(embed) # Append the new embed to the list of embeds
-            await pinboard.edit(embeds=embeds) # Edit the pinboard message with the updated list of embeds
+            pinboard_id = self.pinboards[message.channel.id] 
+            pinboard = await message.channel.fetch_message(pinboard_id) 
+            embeds = pinboard.embeds 
+            embed = Embed(title=message.content, url=message.jump_url) 
+            embeds.append(embed) 
+            await pinboard.edit(embeds=embeds) 
             
-            confirmation = await message.channel.send(f"{message.author.mention}'s pin has been successfully added to {message.channel.name} Pinboard.") # Send a new message to confirm that the message has been added to the pinboard
-            await confirmation.add_reaction("✅") # Add a green checkmark reaction to the confirmation message
+            confirmation = await message.channel.send(f"{message.author.mention}'s pin has been successfully added to {message.channel.name} Pinboard.") 
+            await confirmation.add_reaction("✅") 
             
-            await asyncio.sleep(5) # Wait for 5 seconds
+            await asyncio.sleep(5) 
             
-            prompt = await message.channel.history(limit=2).flatten() # Get the last two messages in the channel, which are the prompt and confirmation messages
-            await prompt[0].delete() # Delete the confirmation message
-            await prompt[1].delete() # Delete the prompt message
+            prompt = await message.channel.history(limit=2).flatten() 
+            for msg in prompt:
+                await msg.delete() 
             
-            await message.delete() # Delete the user's response
+            await message.delete() 
