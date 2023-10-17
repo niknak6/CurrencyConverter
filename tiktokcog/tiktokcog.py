d725d52d935c97f48534424b93c76002db216e7e
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-16 21:25:06 -0400

    Update tiktokcog.py

--- tiktokcog/tiktokcog.py
+++ tiktokcog/tiktokcog.py
@@ -11,7 +11,7 @@ class TikTokCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         # Compile the tiktok pattern only once
-        self.tiktok_pattern = re.compile(r"(?i)(.*?)(https?://)?((\w+)\.)?tiktok.com(?!\/)(.+)(.*)") # Modified line
+        self.tiktok_pattern = re.compile(r"(?i)(.*?)(https?://)?((\w+)\.)?tiktok.com/(.+)(.*)") # Unmodified line
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -24,10 +24,13 @@ class TikTokCog(commands.Cog):
             return
 
         # Add vx in front of tiktok.com in the url, while preserving the protocol, subdomain, and path parts
-        new_url = tiktok_url.group(1) + tiktok_url.group(2) + tiktok_url.group(3) + "vxtiktok.com" + tiktok_url.group(5) + tiktok_url.group(6) # Modified line
+        new_url = tiktok_url.group(1) + tiktok_url.group(2) + tiktok_url.group(3) + "vxtiktok.com/" + tiktok_url.group(5) + tiktok_url.group(6) # Unmodified line
 
         # Get the memo from the message content using the memo group of the tiktok pattern
-        memo = tiktok_url.group(1) # Added line
+        memo = tiktok_url.group(1) # Unmodified line
+
+        # Remove the memo from the new url using a regular expression that matches anything before a slash or a space
+        new_url = re.sub(r".*(?=\/|\s)", "", new_url) # Added line
 
         # Get the user object from the message
         user = message.author
@@ -72,7 +75,7 @@ class TikTokCog(commands.Cog):
             emoji = await guild.create_custom_emoji(name=emoji_name, image=image.read())
 
         # Create a formatted message with the custom emoji, the mention, modified url and memo
-        formatted_message = f"{emoji} {user.mention} shared this TikTok video. {new_url} {memo}" # Modified line
+        formatted_message = f"{emoji} {user.mention} shared this TikTok video. {new_url} {memo}" # Unmodified line
 
         # Repost the formatted message and remove the original message
         await message.channel.send(formatted_message)
