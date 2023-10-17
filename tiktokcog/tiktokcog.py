742fb1e442ec771e54c13972ccb476927d372fd5
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-16 22:22:56 -0400

    Update tiktokcog.py

--- tiktokcog/tiktokcog.py
+++ tiktokcog/tiktokcog.py
@@ -11,7 +11,7 @@ class TikTokCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         # Compile the tiktok pattern only once
-        self.tiktok_pattern = re.compile(r"(?i)(.*?)(https?://)?((\*)\.)?tiktok\.com/(ZM\w+)?(.+)(.*)") # Modified line
+        self.tiktok_pattern = re.compile(r"(?i)(.*?)(https?://)?((\w+)\.)?tiktok.com/(.+)(.*)") # Modified line
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -24,7 +24,7 @@ class TikTokCog(commands.Cog):
             return
 
         # Add vx in front of tiktok.com in the url, while preserving the protocol, subdomain, and path parts
-        new_url = (tiktok_url.group(1) or "") + (tiktok_url.group(2) or "") + (tiktok_url.group(3) or "") + "vxtiktok.com/" + (tiktok_url.group(5) or "") + (tiktok_url.group(6) or "") # Modified line
+        new_url = tiktok_url.group(1) + tiktok_url.group(2) + tiktok_url.group(3) + "vxtiktok.com/" + tiktok_url.group(5) + tiktok_url.group(6) # Modified line
 
         # Get the user object from the message
         user = message.author
