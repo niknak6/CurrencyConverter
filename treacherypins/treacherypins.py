095d20bdd3cdccf822fbf0130c647dc52e2e564b
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-18 16:14:45 -0400

    Update treacherypins.py

--- treacherypins/treacherypins.py
+++ treacherypins/treacherypins.py
@@ -1,118 +1,105 @@
-# Import the required modules
-from redbot.core import commands, checks, Config
+# Import discord and the commands extension
 import discord
+from redbot.core import commands
 
-# Define the cog class
+# Create a class for the cog
 class TreacheryPins(commands.Cog):
-    """A cog that creates pinboards for messages"""
+    """A cog that allows users to pin messages to a channel-specific pinboard."""
 
-    # Initialize the cog
+    # Initialize the cog with the bot instance
     def __init__(self, bot):
         self.bot = bot
-        # Create a config object to store the pinboard data
-        self.config = Config.get_conf(self, identifier=1234567890)
-        # Register the pinboard data as a dict with channel IDs as keys and message IDs as values
-        self.config.register_global(pinboards={})
+        # Create a dictionary to store the pinboard messages by channel id
+        self.pinboards = {}
+        # Create a dictionary to store the pinboard emojis by guild id
+        self.pinboard_emojis = {}
 
-    # Define a command to create a pinboard in the current channel
+    # Create a command to create a pinboard message in the current channel
     @commands.command()
-    @checks.mod_or_permissions(manage_messages=True)
-    async def pinboard(self, ctx):
-        """Create a new pinboard in this channel and pin it"""
-        # Check if there is already a pinboard in this channel
-        pinboards = await self.config.pinboards()
-        if ctx.channel.id in pinboards:
-            await ctx.send("There is already a pinboard in this channel.")
-            return
-        # Create a new message with the title "Pinboard" and an empty description
-        embed = discord.Embed(title="Pinboard", description="")
-        message = await ctx.send(embed=embed)
-        # Pin the message to the channel
-        await message.pin()
-        # Save the message ID as the pinboard for this channel
-        pinboards[ctx.channel.id] = message.id
-        await self.config.pinboards.set(pinboards)
-        await ctx.send("Pinboard created and pinned.")
+    @commands.admin()
+    async def createpinboard(self, ctx):
+        """Create a pinboard message in the current channel."""
+        # Check if there is already a pinboard message in the current channel
+        if ctx.channel.id in self.pinboards:
+            # If yes, send an error message
+            await ctx.send("There is already a pinboard message in this channel.")
+        else:
+            # If no, create a new message with the title "Treachery Pin Board"
+            pinboard = await ctx.send("Treachery Pin Board")
+            # Pin the message
+            await pinboard.pin()
+            # Save the message id in the dictionary by channel id
+            self.pinboards[ctx.channel.id] = pinboard.id
+            # Send a confirmation message
+            await ctx.send("Pinboard message created and pinned.")
 
-    # Define a command to remove the pinboard in the current channel
+    # Create a command to remove the pinboard message from the current channel
     @commands.command()
-    @checks.mod_or_permissions(manage_messages=True)
+    @commands.admin()
     async def removepinboard(self, ctx):
-        """Remove the current pinboard in this channel"""
-        # Check if there is a pinboard in this channel
-        pinboards = await self.config.pinboards()
-        if ctx.channel.id not in pinboards:
-            await ctx.send("There is no pinboard in this channel.")
-            return
-        # Get the message ID of the pinboard
-        message_id = pinboards[ctx.channel.id]
-        # Try to fetch the message from the channel history
-        try:
-            message = await ctx.channel.fetch_message(message_id)
-            # Unpin and delete the message
-            await message.unpin()
-            await message.delete()
-            await ctx.send("Pinboard removed.")
-        except discord.NotFound:
-            # The message was not found, so just delete the data
-            await ctx.send("Pinboard not found. Deleting data.")
-        # Delete the pinboard data for this channel
-        del pinboards[ctx.channel.id]
-        await self.config.pinboards.set(pinboards)
+        """Remove the pinboard message from the current channel."""
+        # Check if there is a pinboard message in the current channel
+        if ctx.channel.id in self.pinboards:
+            # If yes, get the message object by id
+            pinboard = await ctx.channel.fetch_message(self.pinboards[ctx.channel.id])
+            # Unpin the message
+            await pinboard.unpin()
+            # Delete the message
+            await pinboard.delete()
+            # Remove the channel id from the dictionary
+            del self.pinboards[ctx.channel.id]
+            # Send a confirmation message
+            await ctx.send("Pinboard message removed and unpinned.")
+        else:
+            # If no, send an error message
+            await ctx.send("There is no pinboard message in this channel.")
 
-    # Define a listener for reaction add events
+    # Create a command to set the pinboard emoji for the current guild
+    @commands.command()
+    @commands.admin()
+    async def pinboardemoji(self, ctx, emoji: discord.PartialEmoji):
+        """Set the pinboard emoji for the current guild."""
+        # Save the emoji in the dictionary by guild id
+        self.pinboard_emojis[ctx.guild.id] = emoji
+        # Send a confirmation message
+        await ctx.send(f"Pinboard emoji set to {emoji}.")
+
+    # Create a listener for reaction add events
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        """Add a message to the pinboard when reacted with a push pin emoji"""
-        # Check if the reaction is a push pin emoji
-        if payload.emoji.name != "📌":
-            return
-        # Check if the reaction is in a channel with a pinboard
-        pinboards = await self.config.pinboards()
-        if payload.channel_id not in pinboards:
-            return
-        # Try to fetch the message that received the reaction from the channel history
-        try:
-            channel = self.bot.get_channel(payload.channel_id)
-            message = await channel.fetch_message(payload.message_id)
-            # Pass the message to the on_message listener with an explicit argument name
-            await self.on_message(message_to_pin=message)
-        except discord.NotFound:
-            # The message was not found, so do nothing
-            return
-
-    # Define a listener for message events
-    @commands.Cog.listener()
-    async def on_message(self, message_to_pin):
-        """Add an embed of the message content to the pinboard"""
-        # Check if the message is not from the bot and in a channel with a pinboard
-        if message_to_pin.author == self.bot.user:
-            return
-        pinboards = await self.config.pinboards()
-        if message_to_pin.channel.id not in pinboards:
-            return
-        # Get the message ID of the pinboard
-        message_id = pinboards[message_to_pin.channel.id]
-        # Try to fetch the pinboard message from the channel history
-        try:
-            pin_message = await message_to_pin.channel.fetch_message(message_id)
-            # Create an embed of the original message content with author, timestamp, and attachments
-            embed = discord.Embed(description=message_to_pin.content, timestamp=message_to_pin.created_at)
-            embed.set_author(name=message_to_pin.author.display_name, icon_url=message_to_pin.author.avatar_url)
-            if message_to_pin.attachments:
-                embed.set_image(url=message_to_pin.attachments[0].url)
-            # Edit the pinboard message and append the embed to the description
-            pin_embed = pin_message.embeds[0]
-            pin_embed.description += f"\n\n{embed.to_dict()}"
-            await pin_message.edit(embed=pin_embed)
-            # Send a confirmation message and react with a checkmark emoji
-            confirm = await message_to_pin.channel.send("Message added to the pinboard.")
-            await confirm.add_reaction("✅")
-            # Wait for 5 seconds and delete the last two messages in the channel history
-            await asyncio.sleep(5)
-            await message_to_pin.channel.purge(limit=2)
-            # Delete the original message
-            await message_to_pin.delete()
-        except discord.NotFound:
-            # The pinboard message was not found, so do nothing
-            return
+        """Handle reactions to messages."""
+        # Get the channel object by id
+        channel = self.bot.get_channel(payload.channel_id)
+        # Check if the channel has a pinboard message
+        if channel.id in self.pinboards:
+            # Get the guild object by id
+            guild = self.bot.get_guild(payload.guild_id)
+            # Check if the guild has a pinboard emoji
+            if guild.id in self.pinboard_emojis:
+                # Get the emoji object by id
+                emoji = self.pinboard_emojis[guild.id]
+                # Check if the reaction emoji matches the pinboard emoji
+                if payload.emoji == emoji:
+                    # Get the user object by id
+                    user = self.bot.get_user(payload.user_id)
+                    # Check if the user is not a bot and not an admin
+                    if not user.bot and not user.guild_permissions.administrator:
+                        # Get the message object by id
+                        message = await channel.fetch_message(payload.message_id)
+                        # Get the pinboard message object by id
+                        pinboard = await channel.fetch_message(self.pinboards[channel.id])
+                        # Send a prompt message to the user for a description of the message to pin
+                        prompt = await user.send(f"You reacted to a message with {emoji}. Please provide a description of the message to pin.")
+                        # Wait for the user's response
+                        try:
+                            response = await self.bot.wait_for("message", check=lambda m: m.author == user and m.channel == prompt.channel, timeout=60)
+                        except asyncio.TimeoutError:
+                            # If the user does not respond in time, send an error message
+                            await user.send("You did not provide a description in time. Pin aborted.")
+                        else:
+                            # If the user responds, get the description from the message content
+                            description = response.content
+                            # Edit the pinboard message to add a new line with the description and the message link
+                            await pinboard.edit(content=pinboard.content + f"\n{description}: {message.jump_url}")
+                            # Send a confirmation message to the user
+                            await user.send("Pin successfully added!")
