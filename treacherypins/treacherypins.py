6683a6fd70a96357c2531473956b7712c9ce45cf
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-19 18:07:15 -0400

    Update treacherypins.py

--- treacherypins/treacherypins.py
+++ treacherypins/treacherypins.py
@@ -14,19 +14,16 @@ class TreacheryPins(commands.Cog):
     # Initialize the cog with the bot instance
     def __init__(self, bot):
         self.bot = bot
-        # Check if the file exists
-        if not os.path.exists(data_file):
-            # If not, create an empty file
-            open(data_file, "w").close()
-        # Open the file with the mode "r"
-        with open(data_file, "r") as f:
+        # Open the file with the mode "a+"
+        with open(data_file, "a+") as f:
+            # Move the file pointer to the beginning
+            f.seek(0)
             # Try to load the data from the file
             try:
                 self.data = json.load(f)
             except json.decoder.JSONDecodeError:
                 # If the file is not a valid JSON file, write an empty JSON object with the guilds key to the file
-                with open(data_file, "w") as f:
-                    json.dump({"guilds": {}}, f)
+                json.dump({"guilds": {}}, f)
                 # Load the data from the file again
                 self.data = json.load(f)
 
@@ -79,50 +76,50 @@ class TreacheryPins(commands.Cog):
     # Create a command to set the pinboard emoji for the current guild
     @commands.command()
     @commands.has_permissions(administrator=True) # Check for Administrator permission
-    async def pinboardemoji(self, ctx, emoji: str): # Change type annotation to str
+    async def pinboardemoji(self, ctx, emoji: str): # Change type annotation to str 
         """Set the pinboard emoji for the current guild."""
         # Save the emoji in the dictionary by guild id
-        self.data["guilds"][str(ctx.guild.id)]["pinboard_emoji"] = emoji
-        # Save the data to the file
-        with open(data_file, "w") as f:
-            json.dump(self.data, f)
-        # Send a confirmation message
+        self.data["guilds"][str(ctx.guild.id)]["pinboard_emoji"] = emoji 
+        # Save the data to the file 
+        with open(data_file, "w") as f: 
+            json.dump(self.data, f) 
+        # Send a confirmation message 
         await ctx.send(f"Pinboard emoji set to {emoji}.")
 
-    # Create a listener for reaction add events
-    @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user): # Change event name to on_reaction_add
-        """Handle reactions to messages."""
-        # Get the channel object from the reaction
-        channel = reaction.message.channel # Change payload to reaction
-        # Check if the channel has a pinboard message
-        if channel.id in self.data["guilds"].get(str(channel.guild.id), {})["pinboards"]: # Use dict.get method with default value
-            # Get the guild object from the channel
-            guild = channel.guild # Change payload to channel
-            # Check if the guild has a pinboard emoji
-            if guild.id in self.data["guilds"]:
-                # Get the emoji object by id
+    # Create a listener for reaction add events 
+    @commands.Cog.listener() 
+    async def on_reaction_add(self, reaction, user):  # Change event name to on_reaction_add 
+        """Handle reactions to messages.""" 
+        # Get the channel object from the reaction 
+        channel = reaction.message.channel  # Change payload to reaction 
+        # Check if the channel has a pinboard message 
+        if channel.id in self.data["guilds"].get(str(channel.guild.id), {})["pinboards"]:  # Use dict.get method with default value 
+            # Get the guild object from the channel 
+            guild = channel.guild  # Change payload to channel 
+            # Check if the guild has a pinboard emoji 
+            if guild.id in self.data["guilds"]: 
+                # Get the emoji object by name 
                 emoji = self.data["guilds"][str(guild.id)]["pinboard_emoji"]  # Change this line 
-                # Check if the reaction emoji matches the pinboard emoji
-                if reaction.emoji.name == emoji.strip(":"): # Change payload to reaction
-                    # Check if the user is not a bot and not an admin
-                    if not user.bot and not user.guild_permissions.administrator: # Use user object directly
-                        # Get the message object from the reaction
-                        message = reaction.message # Change payload to reaction
-                        # Get the pinboard message object by id
-                        pinboard = await channel.fetch_message(self.data["guilds"][str(channel.guild.id)]["pinboards"][str(channel.id)])
-                        # Send a prompt message to the user for a description of the message to pin
-                        prompt = await user.send(f"You reacted to a message with {emoji}. Please provide a description of the message to pin.")
-                        # Wait for the user's response
-                        try:
-                            response = await self.bot.wait_for("message", check=lambda m: m.author == user and m.channel == prompt.channel, timeout=60)
-                        except asyncio.TimeoutError:
-                            # If the user does not respond in time, send an error message
-                            await user.send("You did not provide a description in time. Pin aborted.")
-                        else:
-                            # If the user responds, get the description from the message content
-                            description = response.content
-                            # Edit the pinboard message to add a new line with the description and the message link
-                            await pinboard.edit(content=pinboard.content + f"\n{description}: {message.jump_url}")
+                # Check if the reaction emoji matches the pinboard emoji 
+                if reaction.emoji.name == emoji.strip(":"):  # Change payload to reaction 
+                    # Check if the user is not a bot and not an admin 
+                    if not user.bot and not user.guild_permissions.administrator if user else False:  # Use user object directly and handle DM case
+                        # Get the message object from the reaction 
+                        message = reaction.message  # Change payload to reaction 
+                        # Get the pinboard message object by id 
+                        pinboard = await channel.fetch_message(self.data["guilds"][str(channel.guild.id)]["pinboards"][str(channel.id)]) 
+                        # Send a prompt message to the user for a description of the message to pin 
+                        prompt = await user.send(f"You reacted to a message with {emoji}. Please provide a description of the message to pin.") 
+                        # Wait for the user's response 
+                        try: 
+                            response = await self.bot.wait_for("message", check=lambda m: m.author == user and m.channel == prompt.channel, timeout=60) 
+                        except asyncio.TimeoutError: 
+                            # If the user does not respond in time, send an error message 
+                            await user.send("You did not provide a description in time. Pin aborted.") 
+                        else: 
+                            # If the user responds, get the description from the message content 
+                            description = response.content 
+                            # Edit the pinboard message to add a new line with the description and the message link 
+                            await pinboard.edit(content=pinboard.content + f"\n{description}: {message.jump_url}") 
                             # Send a confirmation message to the user
                             await user.send("Pin successfully added!")
