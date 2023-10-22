e357e0e5d9f40937f187a9f89e45c29163d0417b
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-22 02:25:27 -0400

    Update pinextender.py

--- pinextender/pinextender.py
+++ pinextender/pinextender.py
@@ -3,6 +3,13 @@ from redbot.core import commands, checks
 import discord
 import urllib.parse # Added this module to parse URLs
 
+# Define a custom function to validate URLs
+def is_url(string):
+    # Try to parse the string as a URL
+    result = urllib.parse.urlparse(string)
+    # Return True if the string has a valid scheme and netloc, False otherwise
+    return result.scheme and result.netloc
+
 # Define the cog class
 class PinExtender(commands.Cog):
     """A cog that extends the pin limit of a channel by using a pinned message as a container for other pins."""
@@ -48,8 +55,8 @@ class PinExtender(commands.Cog):
 
             # Get the message link and description of the last pinned message
             message_link = last_pinned_message.jump_url
-            # Modified this line to check if the message content is only a valid URL and use the :paperclip: emoji accordingly
-            message_description = ":paperclip:" if validators.url(last_pinned_message.content) else (last_pinned_message.content[:20] + "..." if len(last_pinned_message.content) > 20 else last_pinned_message.content)
+            # Modified this line to use the custom function is_url instead of validators.url
+            message_description = ":paperclip:" if is_url(last_pinned_message.content) else (last_pinned_message.content[:20] + "..." if len(last_pinned_message.content) > 20 else last_pinned_message.content)
 
             # Add the message link and description to the bottom of the extended pins message
             await extended_pins_message.edit(content=f"{extended_pins_message.content}\n{message_link} - {message_description}")
