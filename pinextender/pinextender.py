cd86cc2eae1b8d0dd872a677955ca944e134269c
niknak6 <113207452+niknak6@users.noreply.github.com>
2023-10-22 01:12:10 -0400

    Update pinextender.py

--- pinextender/pinextender.py
+++ pinextender/pinextender.py
@@ -3,34 +3,34 @@ from redbot.core import commands
 from discord.ext import tasks
 import discord
 import logging
-import datetime # Import the datetime module
-import urllib.parse # Import the urllib.parse module
+import datetime
+import urllib.parse
 
 # Define constants
-PIN_LIMIT = 50 # The pin limit of a channel, as per Discord's documentation
-EXTENDED_PINS_CONTENT = "**Extended Pins**\n" # The content of the extended pins message
+PIN_LIMIT = 50
+EXTENDED_PINS_CONTENT = "**Extended Pins**\n"
 
 # Define a logger
 log = logging.getLogger("red.pinextender")
 
 # Define a check for bot permissions
-def bot_has_manage_messages():
+def bot_has_manage_messages() -> Callable[[commands.Context], Awaitable[bool]]:
     """A check that ensures that the bot has manage messages permission."""
-    async def predicate(ctx):
+    async def predicate(ctx: commands.Context) -> bool:
         if not ctx.guild:
             return False
         return ctx.channel.permissions_for(ctx.guild.me).manage_messages
     return commands.check(predicate)
 
 # Define a check for guild only
-def guild_only():
+def guild_only() -> Callable[[commands.Context], Awaitable[bool]]:
     """A check that ensures that the command is invoked in a guild."""
-    async def predicate(ctx):
+    async def predicate(ctx: commands.Context) -> bool:
         return ctx.guild is not None
     return commands.check(predicate)
 
 # Define a check for owner only
-def owner_only():
+def owner_only() -> Callable[[commands.Context], Awaitable[bool]]:
     """A check that ensures that the command is invoked by the owner."""
     return commands.check(commands.is_owner())
 
@@ -39,6 +39,7 @@ class PinExtender(commands.Cog):
     """A cog that extends the pin limit of a channel by using a pinned message as a container for more pins."""
 
     def __init__(self, bot: commands.Bot):
+        """Initializes the cog with the bot object and an empty dictionary for extended pins."""
         self.bot = bot
         self.extended_pins: Dict[int, int] = {} # A dictionary that maps channel IDs to extended pins messages IDs
 
@@ -73,22 +74,13 @@ class PinExtender(commands.Cog):
     @bot_has_manage_messages()
     async def on_guild_channel_pins_update(self, channel: discord.TextChannel, last_pin: datetime.datetime):
         """Updates the extended pins message when a new pin is added to a channel."""
-        # Get the channel ID from the channel object
-        channel_id = channel.id
-        
         # Check if the channel has an extended pins message
-        if channel_id in self.extended_pins:
-            # Get the guild ID from the channel object
-            guild_id = channel.guild.id
-            
-            # Get the guild object from its ID
-            guild = self.bot.get_guild(guild_id)
-            
+        if channel.id in self.extended_pins:
             # Try to fetch the extended pins message from the channel
             try:
-                message = await channel.fetch_message(self.extended_pins[channel_id])
+                message = await channel.fetch_message(self.extended_pins[channel.id])
             except discord.NotFound: # Handle the case when the message is not found
-                del self.extended_pins[channel_id] # Delete the entry from the dictionary if the message is deleted
+                del self.extended_pins[channel.id] # Delete the entry from the dictionary if the message is deleted
                 return # Return from the method if the message is deleted
             except discord.Forbidden: # Handle the case when the bot lacks permissions
                 log.error(f"I do not have permission to access pinned messages in {channel}.") # Log an error message to inform the owner
@@ -106,7 +98,7 @@ class PinExtender(commands.Cog):
                 if new_pin and new_pin.id != message.id: 
 
                     # Get or fetch (if not cached) who pinned it from their ID using the audit log entry for the pin action
-                    audit_log_entries = guild.audit_logs(action=discord.AuditLogAction.message_pin) # Get the audit log entries for the pin action
+                    audit_log_entries = channel.guild.audit_logs(action=discord.AuditLogAction.message_pin) # Get the audit log entries for the pin action
                     async for entry in audit_log_entries: # Use an async for loop to iterate over the async generator
                         if entry.target.id == new_pin.id: # Check if the entry matches the new pin message ID
                             pinner = entry.user or await self.bot.fetch_user(entry.user.id) # Get the user object from the audit log entry
@@ -128,7 +120,7 @@ class PinExtender(commands.Cog):
                         await new_pin.unpin()
                     except discord.NotFound:
                         # Handle the case when the message is not found
-                        log.error(f"The new pin {new_pin} was not found in {channel}.") # Log an error message to inform the owner
+                        log.warning(f"The new pin {new_pin} was not found in {channel}.") # Log a warning message to inform the owner
                     except discord.Forbidden:
                         # Handle the case when the bot lacks permissions
                         log.error(f"I do not have permission to unpin messages in {channel}.") # Log an error message to inform the owner
